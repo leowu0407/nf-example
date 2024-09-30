d42983078a9ea325196a99792fcd972dc6359640
ubuntu <ubuntu@lab6.myguest.virtualbox.org>
2024-09-30 18:20:43 +0800

    refactor: use handler function for SAO Route

--- internal/sbi/api_sao.go
+++ internal/sbi/api_sao.go
@@ -21,9 +21,7 @@ func (s *Server) getSAORoute() []Route {
 			Name:    "Welcome to SAO!",
 			Method:  http.MethodGet,
 			Pattern: "/",
-			APIFunc: func(c *gin.Context) {
-				c.JSON(http.StatusOK, "Welcome to SAO!")
-			},
+			APIFunc: s.WelcomeSAO,
 			// Use
 			// curl -X GET http://127.0.0.163:8000/sao/ -w "\n"
 		},
@@ -31,9 +29,7 @@ func (s *Server) getSAORoute() []Route {
 			Name:    "SAO Character List",
 			Method:  http.MethodGet,
 			Pattern: "/list",
-			APIFunc: func(c *gin.Context) {
-				c.JSON(http.StatusOK, "SAO Character List : "+strings.Join(list.Characters, ", "))
-			},
+			APIFunc: s.ListSAOCharacter,
 			// Use
 			// curl -X GET http://127.0.0.163:8000/sao/list -w "\n"
 		},
@@ -41,22 +37,32 @@ func (s *Server) getSAORoute() []Route {
 			Name:    "Create SAO Character",
 			Method:  http.MethodPost,
 			Pattern: "/character",
-			APIFunc: func(c *gin.Context) {
-				var character struct {
-					Name string `json:"name"`
-				}
-
-				err := c.ShouldBindJSON(&character)
-				if err != nil {
-					c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-					return
-				}
-
-				list.Characters = append(list.Characters, character.Name)
-				c.JSON(http.StatusOK, "Hello "+character.Name+"!")
-			},
+			APIFunc: s.CreateSAOCharacter,
 			// Use
 			// curl -X POST http://127.0.0.163:8000/sao/character -d '{"name": "Kirito"}' -w "\n"
 		},
 	}
+}
+
+func (s *Server) WelcomeSAO(c *gin.Context){
+	c.String(http.StatusOK, "Welcome to SAO!")
+}
+
+func (s *Server) ListSAOCharacter(c *gin.Context){
+	c.String(http.StatusOK, "SAO Character List : "+strings.Join(list.Characters, ", "))
+}
+
+func (s *Server) CreateSAOCharacter(c *gin.Context){
+	var character struct {
+		Name string `json:"name"`
+	}
+
+	err := c.ShouldBindJSON(&character)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	list.Characters = append(list.Characters, character.Name)
+	c.String(http.StatusOK, "Hello "+character.Name+"!")
 }
